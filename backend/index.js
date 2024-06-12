1a751006acc7b21af355f0fadac5301814f3be6a
mai033 <145622463+mai033@users.noreply.github.com>
2024-06-11 22:14:11 -0700

    update cors

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,55 @@
+// require('dotenv').config();
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const cors = require('cors');
+
+// const ticketRoutes = require('./routes/ticketRoutes');
+
+// const app = express();
+// app.use(cors({ origin: '*' }));
+// app.use(express.json());
+
+// mongoose
+//   .connect(process.env.MONGODB_URI)
+//   .then(() => console.log('MongoDB connected'))
+//   .catch((err) => console.error(err));
+
+// app.get('/', (req, res) => {
+//   res.send('Hey this is my API running 🥳');
+// });
+
+// app.use('/tickets', ticketRoutes);
+
+// app.listen(4000, () => {
+//   console.log('Server is running on port 4000');
+// });
+
+// require('dotenv').config();
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const cors = require('cors');
+
+// const ticketRoutes = require('./routes/ticketRoutes');
+
+// const app = express();
+// app.use(cors({ origin: '*' }));
+// app.use(express.json());
+
+// mongoose
+//   .connect(process.env.MONGODB_URI)
+//   .then(() => console.log('MongoDB connected'))
+//   .catch((err) => console.error(err));
+
+// app.get('/', (req, res) => {
+//   res.send('Hey this is my API running 🥳');
+// });
+
+// app.use('/tickets', ticketRoutes);
+
+// app.listen(4000, () => {
+//   console.log('Server is running on port 4000');
+// });
+
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
@@ -6,7 +58,24 @@ const cors = require('cors');
 const ticketRoutes = require('./routes/ticketRoutes');
 
 const app = express();
-app.use(cors({ origin: '*' }));
+
+const allowedOrigins = [
+  'https://zealthyhelpdesk-dahlias-projects-2ae6059a.vercel.app/',
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    optionsSuccessStatus: 200,
+  })
+);
+
 app.use(express.json());
 
 mongoose
@@ -23,3 +92,4 @@ app.use('/tickets', ticketRoutes);
 app.listen(4000, () => {
   console.log('Server is running on port 4000');
 });
+
